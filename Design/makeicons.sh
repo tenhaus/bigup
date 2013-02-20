5d0aac8e009098401d0bb937ab0720a6206e3cbe
Chris Hayen <chayen@gmail.com>
2013-02-20 11:39:42 -0500

    Updated assets

--- Design/makeicons.sh
+++ Design/makeicons.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 rm icons.iconset/*.png
 
-convert icon_second.png -resize 16x16 icons.iconset/icon_16x16.png
-convert icon_second.png -resize 32x32 icons.iconset/icon_16x16@2x.png
+convert icon.png -resize 16x16 icons.iconset/icon_16x16.png
+convert icon.png -resize 32x32 icons.iconset/icon_16x16@2x.png
 
-convert icon_second.png -resize 32x32 icons.iconset/icon_32x32.png
-convert icon_second.png -resize 64x64 icons.iconset/icon_32x32@2x.png
+convert icon.png -resize 32x32 icons.iconset/icon_32x32.png
+convert icon.png -resize 64x64 icons.iconset/icon_32x32@2x.png
 
-convert icon_second.png -resize 128x128 icons.iconset/icon_128x128.png
-convert icon_second.png -resize 256x256 icons.iconset/icon_128x128@2x.png
+convert icon.png -resize 128x128 icons.iconset/icon_128x128.png
+convert icon.png -resize 256x256 icons.iconset/icon_128x128@2x.png
 
-convert icon_second.png -resize 256x256 icons.iconset/icon_256x256.png
-convert icon_second.png -resize 512x512 icons.iconset/icon_256x256@2x.png
+convert icon.png -resize 256x256 icons.iconset/icon_256x256.png
+convert icon.png -resize 512x512 icons.iconset/icon_256x256@2x.png
 
-convert icon_second.png -resize 512x512 icons.iconset/icon_512x512.png
-convert icon_second.png -resize 1024x1024 icons.iconset/icon_512x512@2x.png
+convert icon.png -resize 512x512 icons.iconset/icon_512x512.png
+convert icon.png -resize 1024x1024 icons.iconset/icon_512x512@2x.png
 
 iconutil -c icns icons.iconset -o ../sexy-wallpaper/icons.icns
